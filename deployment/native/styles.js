efc3456eed559e782774f1f506bc37ed10e15866
philzilla <dev@philippeyim.fr>
2021-12-03 11:53:23 +0100

    Ajoute le module Billets

--- deployment/native/styles.js
+++ deployment/native/styles.js
@@ -2,10 +2,11 @@ import * as style0 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/t
 import * as style1 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/nanoflowcommons/native/main";
 import * as style2 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/webactions/native/main";
 import * as style3 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/atlas_core/native/main";
-import * as style4 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/myfirstmodule/native/main";
+import * as style4 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/tableaudebord/native/main";
 import * as style5 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/visiteur/native/main";
-import * as style6 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/theme/native/main";
+import * as style6 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/themesource/billets/native/main";
+import * as style7 from "C:/Users/Serveur/Documents/Mendix/ecommerceCours-main/theme/native/main";
 
 import { flatten } from "mendix/native";
 
-module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7]);
